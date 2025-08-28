8a3061f29a5b527a0b5340029a61cbb87ed78db1
BrainCellsoptional <olivernicholls27@gmail.com>
2025-08-28 20:08:24 +1200

    update

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,6 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-<<<<<<< HEAD:vite.config.js
-  base: '/scp_react/',
-=======
-  base: '/scp_react',
->>>>>>> 4b86f77 (Updated project structure and assets):app/vite.config.js
+  base: '/react_scp/',
 })
 
